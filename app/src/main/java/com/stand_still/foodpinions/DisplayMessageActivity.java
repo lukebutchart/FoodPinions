ed30611c4c8a67f1e7baa6871421169f84f3e494
Luke Butchart <luke.butchart@gmail.com>
2016-09-03 13:09:31 +0100

    Finished basic tutorial

--- app/src/main/java/com/stand_still/foodpinions/DisplayMessageActivity.java
+++ app/src/main/java/com/stand_still/foodpinions/DisplayMessageActivity.java
@@ -1,7 +1,12 @@
 package com.stand_still.foodpinions;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
     }
 }
