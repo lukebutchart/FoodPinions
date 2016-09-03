ab0e9348fd8942ae7534afef186ca2ffd7bdf9ee
Luke Butchart <luke.butchart@gmail.com>
2016-09-03 12:37:48 +0100

    Created DisplayMessageActivity

--- app/src/main/java/com/stand_still/foodpinions/MainActivity.java
+++ app/src/main/java/com/stand_still/foodpinions/MainActivity.java
@@ -1,9 +1,14 @@
 package com.stand_still.foodpinions;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.stand_still.foodpinions.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Test comment - from laptop
+    // Called when the user clicks the Send button
+    // In order for this to work: Must be public, must return void, must have View as only parameter
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
