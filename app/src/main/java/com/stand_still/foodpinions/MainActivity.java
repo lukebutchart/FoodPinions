03671c64f735ea7da31ee61e247d5a67ff39d684
Luke Butchart <luke.butchart@gmail.com>
2016-09-05 22:24:25 +0100

    Small amount of testing in MainActivity. Can't figure out problem, commented it out.

--- app/src/main/java/com/stand_still/foodpinions/MainActivity.java
+++ app/src/main/java/com/stand_still/foodpinions/MainActivity.java
@@ -1,14 +1,18 @@
 package com.stand_still.foodpinions;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.stand_still.foodpinions.MESSAGE";
+
+//    Button sendButton = (Button) findViewById(R.id.send_button);      // What's the issue here?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     // Called when the user clicks the Send button
     // In order for this to work: Must be public, must return void, must have View as only parameter
     public void sendMessage(View view){
+//        sendButton.setBackgroundColor(Color.CYAN);
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
